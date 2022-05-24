62459319b2230a604384af077c32ddddeec5e9bb
FabianGuarascio <guarascio.fabiand@gmail.com>
2022-05-24 16:59:23 -0300

    agregado un icono de fontAwsome

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import classes from './Header.module.css'
+import { BiCameraMovie } from "react-icons/bi";
  
 
 function Header() {
   return (
     <header className={classes.topContainer}>
+      
       <nav className={classes.container}>
         <Link to='/'>
           
           <span className='span animate__animated animate__shakeY '>Home</span>
         </Link>
-        <Link to='/list'>
-        <span>Movies</span>
+        <Link to='/list' className={classes.link_movies}>
+      
+      
+        <span>Movies</span> <BiCameraMovie className={classes.icon}/>
         </Link>
         <Link to='/contact'>
           <span>Contact</span>
         </Link>
+        
       </nav>
     </header>
   )
